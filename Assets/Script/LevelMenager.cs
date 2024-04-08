217b1d17b96e67e938d235f60b7c4a78a9e871b3
Marek <mareczekblaszczyk@gmail.com>
2024-04-08 10:37:47 +0200

    Błąd

--- Assets/Script/LevelMenager.cs
+++ Assets/Script/LevelMenager.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     Transform player;
-    //odleg³oœc od koñca poziomu
+   
     public float levelExitDistance = 100;
-    //punkt koñca poziomu
+   
     public Vector3 exitPosition;
-    // Start is called before the first frame update
+    public GameObject exitPrefab;
+    
+    public bool levelComplete = false;
+    
+    public bool levelFailed = false;
+    
     void Start()
     {
         //znajdz gracza
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        //wylosuj pozycjê na kole o œrednicy 100 jednostek
-        Vector2 spawnCircle = Random.insideUnitCircle; //losowa pozycja x,y wewn¹trz ko³a o r=1
-        //chcemy tylko pozycjê na okrêgu, a nie wewn¹trz ko³a
-        spawnCircle = spawnCircle.normalized; //pozycje x,y w odleg³oœci 1 od œrodka
-        spawnCircle *= levelExitDistance; //pozycja x,y w odleg³oœci 100 od œrodka
-        //konwertujemy do Vector3
-        //podstawiamy: x=x, y=0, z=y
+        
+        Vector2 spawnCircle = Random.insideUnitCircle;
+        
+        spawnCircle = spawnCircle.normalized; 
+        spawnCircle *= levelExitDistance; 
+        
         exitPosition = new Vector3(spawnCircle.x, 0, spawnCircle.y);
+        Instantiate(exitPrefab, exitPosition, Quaternion.identity);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
 
--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -3,87 +3,105 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float rotationSpeed = 100f;
     public float flySpeed = 5f;
+   
+    GameObject levelManagerObject;
+    
+    float shieldCapacity = 1;
 
-    GameObject levelMenagerObject;
-
-    float shieldCapacity =1;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
-        levelMenagerObject = GameObject.Find("levelMenager");
+        levelManagerObject = GameObject.Find("LevelManager");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        //dodaj do wspólrzednych wartoœæ x=1, y=0, z=0 pomnozone przez czas
-        //mierzony w sekundach od osztatniej klatki
-        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        
+        Vector3 movement = transform.forward;
+       
+        movement *= Time.deltaTime;
+      
+        movement *= Input.GetAxis("Vertical");
+      
+        movement *= flySpeed;
+     
 
-        //prezentacja dzia³anie wyg³adzonego sterowania (emulacji joystika)
-        //Debug.Log(Input, GetAxis("Vertival"));
-
-        //sterowanie predkoscia
-        //stworz nowy wektor przesuniecia o wartosc 1 do przodu
-        Vector3 movment = transform.forward;
-        //pomnorzyc przez czas od ostatniej klatki
-        movment *= Time.deltaTime;
-        //pomnoz go przez "wychylenie joysticka"
-        movment *= Input.GetAxis("Vertical");
-        //pomnoz przez predkosc lotu
-        movment *= flySpeed;
-        //dodaj ruch do obiektu
-        //---transform.position += movment;
+       
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(movment, ForceMode.VelocityChange);
+       
+        rb.AddForce(movement, ForceMode.VelocityChange);
 
-        //obrot
 
-        //modyfikuj oœ "Y" obiektu player
+        
         Vector3 rotation = Vector3.up;
-        //przemonz przez czas
+       
         rotation *= Time.deltaTime;
-        //przemnoz przez klawiature
+        
         rotation *= Input.GetAxis("Horizontal");
-        //pomnoz przez prêdkoœæ obrotu
+       
         rotation *= rotationSpeed;
-
-        //dodaj obrot do obiektu
-        //nie mozemy uzyc += poniewaz unity uzywa quaterenionow do zapisu rotacji
+       
         transform.Rotate(rotation);
         UpdateUI();
     }
 
     private void UpdateUI()
     {
-        Vector3 target = levelMenagerObject.GetComponent<LevelManager>().exitPosition;
-
+        
+        Vector3 target = levelManagerObject.GetComponent<LevelManager>().exitPosition;
+      
         transform.Find("NavUI").Find("TargetMarker").LookAt(target);
-
+       
         TextMeshProUGUI shieldText =
             GameObject.Find("Canvas").transform.Find("ShieldCapacityText").GetComponent<TextMeshProUGUI>();
-        shieldText.text = "Shield:" + (shieldCapacity * 100).ToString() + "%";
+        shieldText.text = " Shield: " + (shieldCapacity * 100).ToString() + "%";
+
+        
+        if (levelManagerObject.GetComponent<LevelManager>().levelComplete)
+        {
+           
+            GameObject.Find("Canvas").transform.Find("LevelCompleteScreen").gameObject.SetActive(true);
+        }
+       
+        if (levelManagerObject.GetComponent<LevelManager>().levelFailed)
+        {
+           
+            GameObject.Find("Canvas").transform.Find("GameOverScreen").gameObject.SetActive(true);
+        }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-
+        
         if (collision.collider.transform.CompareTag("Asteroid"))
         {
+
             Transform asteroid = collision.collider.transform;
-
+           
             Vector3 shieldForce = asteroid.position - transform.position;
-
+          
             asteroid.GetComponent<Rigidbody>().AddForce(shieldForce * 5, ForceMode.Impulse);
             shieldCapacity -= 0.25f;
-            //Debug.Log("Boom!");
-
-            //Time.timeScale = 0;
+            if (shieldCapacity <= 0)
+            {
+                
+                levelManagerObject.GetComponent<LevelManager>().levelFailed = true;
+            }
         }
-    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.transform.CompareTag("LevelExit"))
+        {
+            
+            levelManagerObject.GetComponent<LevelManager>().levelComplete = true;
+        }
     }
 }
